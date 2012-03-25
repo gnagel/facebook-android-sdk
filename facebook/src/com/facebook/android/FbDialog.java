c0e97f5abc84983628f3ea03cb1dc06cd46ce21f
gnagel <glenn@mercury-wireless.com>
2012-03-25 14:36:55 -0400

    Hacking projects for new Android SDK...

--- facebook/src/com/facebook/android/FbDialog.java
+++ facebook/src/com/facebook/android/FbDialog.java
@@ -1,12 +1,9 @@
 /*
  * Copyright 2010 Facebook, Inc.
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,6 +20,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Typeface;
@@ -85,8 +83,7 @@ public class FbDialog extends Dialog {
 
 
 		@Override
-		public void onReceivedError(final WebView view, final int errorCode, final String description,
-				final String failingUrl) {
+		public void onReceivedError(final WebView view, final int errorCode, final String description, final String failingUrl) {
 			super.onReceivedError(view, errorCode, description, failingUrl);
 			mListener.onError(new DialogError(description, errorCode, failingUrl));
 			dismiss();
@@ -134,31 +131,42 @@ public class FbDialog extends Dialog {
 
 	static final float[]					DIMENSIONS_DIFF_LANDSCAPE	= { 20, 60 };
 
+
 	static final float[]					DIMENSIONS_DIFF_PORTRAIT	= { 40, 60 };
+
 
 	static final String						DISPLAY_STRING				= "touch";
 
+
 	static final int						FB_BLUE						= 0xFF6D84B4;
+
 
 	static final String						FB_ICON						= "icon.png";
 
+
 	static final int						MARGIN						= 4;
+
 
 	static final int						PADDING						= 2;
 
-	static final FrameLayout.LayoutParams	WRAP						= new FrameLayout.LayoutParams(
-																				ViewGroup.LayoutParams.WRAP_CONTENT,
-																				ViewGroup.LayoutParams.WRAP_CONTENT);
+
+	static final FrameLayout.LayoutParams	WRAP						= new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
 
 	private LinearLayout					mContent;
 
+
 	private final DialogListener			mListener;
+
 
 	private ProgressDialog					mSpinner;
 
+
 	private TextView						mTitle;
 
+
 	private final String					mUrl;
+
 
 	private WebView							mWebView;
 
@@ -198,7 +206,12 @@ public class FbDialog extends Dialog {
 
 	private void setUpTitle() {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		final Drawable icon = getContext().getResources().getDrawable(R.drawable.facebook_icon);
+
+		final Context context = getContext();
+		final Resources resources = context.getResources();
+
+		final int iconId = resources.getIdentifier("facebook_icon", "drawable", context.getPackageName());
+		final Drawable icon = resources.getDrawable(iconId);
 		mTitle = new TextView(getContext());
 		mTitle.setText("Facebook");
 		mTitle.setTextColor(Color.WHITE);
@@ -238,11 +251,9 @@ public class FbDialog extends Dialog {
 		final Display display = getWindow().getWindowManager().getDefaultDisplay();
 		final float scale = getContext().getResources().getDisplayMetrics().density;
 		final int orientation = getContext().getResources().getConfiguration().orientation;
-		final float[] dimensions = orientation == Configuration.ORIENTATION_LANDSCAPE ? FbDialog.DIMENSIONS_DIFF_LANDSCAPE
-				: FbDialog.DIMENSIONS_DIFF_PORTRAIT;
+		final float[] dimensions = orientation == Configuration.ORIENTATION_LANDSCAPE ? FbDialog.DIMENSIONS_DIFF_LANDSCAPE : FbDialog.DIMENSIONS_DIFF_PORTRAIT;
 
-		mWebView.setLayoutParams(new LinearLayout.LayoutParams(display.getWidth()
-				- (int) (dimensions[0] * scale + 0.5f), display.getHeight() - (int) (dimensions[1] * scale + 0.5f)));
+		mWebView.setLayoutParams(new LinearLayout.LayoutParams(display.getWidth() - (int) (dimensions[0] * scale + 0.5f), display.getHeight() - (int) (dimensions[1] * scale + 0.5f)));
 		mContent.addView(mWebView);
 	}
 }
